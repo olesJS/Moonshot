0d232690c686f2e02a78c941a9f4b656a6339eb4
Oleksii <85940148+olesJS@users.noreply.github.com>
2023-06-25 17:38:06 +0300

    v1

---
+++ Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by Олексій Якимчук on 22.06.2023.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
---
+++ AstronautView.swift
@@ -0,0 +1,43 @@
+//
+//  AstronautView.swift
+//  Moonshot
+//
+//  Created by Олексій Якимчук on 25.06.2023.
+//
+
+import SwiftUI
+
+struct AstronautView: View {
+    let astronaut: Astronaut
+    
+    var body: some View {
+        ScrollView {
+            VStack {
+                Image(astronaut.id)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(maxWidth: .infinity)
+                    .clipShape(RoundedRectangle(cornerRadius: 15))
+                    .padding(.horizontal)
+                    
+                Divider()
+                    .padding()
+                
+                Text(astronaut.description)
+                    .padding()
+            }
+        }
+        .background(.darkBackground)
+        .navigationTitle(astronaut.name)
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+struct AstronautView_Previews: PreviewProvider {
+    static let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
+    
+    static var previews: some View {
+        AstronautView(astronaut: astronauts["armstrong"]!)
+            .preferredColorScheme(.dark)
+    }
+}
